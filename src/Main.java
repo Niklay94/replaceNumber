3acb5b8c7762d96b64dbeb70518eceb947ad808e
NikolayHomePC <myEmail@example.com>
2020-08-24 00:28:28 +0300

    add baseDir

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        String baseDir = new File("").getAbsolutePath();
         ArrayList<String> list = new ArrayList<>();
         System.out.println("Hello World!");
-        String fileName = "E:/работа/ReplaceNumber(Java)_1/test.txt";
+        String fileName = baseDir+"/test.txt";
         ReadFile.readFile(fileName, list);
         //System.out.println("list: "+list);
         Numeration.replace(list);
